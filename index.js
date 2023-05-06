52b49fd59b7e05cc4c132db439887f1255fe8403
nokcha09 <nokcha0928@gmail.com>
2023-05-06 12:02:54 +0900

    dev set

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 import dotenv from "dotenv";
 import express from "express";
-import firebaseDB from "./firebase.js";
 
 dotenv.config();
 
 const app = express();
 /** for Nomal Hosting setUP */
-// const port = process.env.SERVICE_PORT;
+const port = process.env.SERVICE_PORT;
 
-app.get("/", function (req, res) {});
+app.get("/", function (req, res) {
+  res.status(200).send("Dev now");
+});
 
 /** for Nomal Hosting setUP */
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
 /** for FirebaseHosting service */
-exports.app = functions.https.onRequest(app);
+// exports.app = functions.https.onRequest(app);
